fb511cbe0696f3338c12cf36e29b126476796a4a
yahirokendy <kendyyahiro@Hotmail.com>
2021-10-02 12:22:58 -0400

    subindo a FK de Vaccinated

--- app/src/main/java/com/example/trabalho1/VaccineVaccinated/Vaccinated/Vaccinated.java
+++ app/src/main/java/com/example/trabalho1/VaccineVaccinated/Vaccinated/Vaccinated.java
@@ -1,9 +1,13 @@
 package com.example.trabalho1.VaccineVaccinated.Vaccinated;
 
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
-@Entity public class Vaccinated {
+import com.example.trabalho1.VaccineVaccinated.Vaccine.Vaccine;
+
+@Entity(foreignKeys = @ForeignKey(entity = Vaccine.class, parentColumns = "vacinaId", childColumns = "vacinaId"))
+public class Vaccinated {
     @PrimaryKey(autoGenerate = true) int numVacinado;
 
     public int vacinaId;
