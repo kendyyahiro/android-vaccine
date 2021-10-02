907fee1748d03a036d0a7189582519aed3b35a48
yahirokendy <kendyyahiro@Hotmail.com>
2021-10-02 12:24:01 -0400

    Correção e melhoria no VaccineVaccinatedDatabase

--- app/src/main/java/com/example/trabalho1/VaccineVaccinated/VaccineVaccinatedDatabase.java
+++ app/src/main/java/com/example/trabalho1/VaccineVaccinated/VaccineVaccinatedDatabase.java
@@ -10,18 +10,21 @@ import com.example.trabalho1.VaccineVaccinated.Vaccinated.IVaccinatedDao;
 import com.example.trabalho1.VaccineVaccinated.Vaccinated.Vaccinated;
 import com.example.trabalho1.VaccineVaccinated.Vaccine.IVaccineDao;
 import com.example.trabalho1.VaccineVaccinated.Vaccine.Vaccine;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 @Database(entities = {Vaccine.class, Vaccinated.class}, version = 1)
-public abstract class VaccineVaccinedDatabase extends RoomDatabase {
+public abstract class VaccineVaccinatedDatabase extends RoomDatabase {
+    private static volatile VaccineVaccinatedDatabase instance;
+
+    public static synchronized VaccineVaccinatedDatabase getInstance(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    VaccineVaccinatedDatabase.class, "PostoX").build();
+        }
+        return instance;
+    }
+
     public abstract IVaccineDao vaccineDao();
     public abstract IVaccinatedDao vaccinatedDao();
-    private static volatile VaccineVaccinedDatabase instance;
-
-   public static synchronized VaccineVaccinedDatabase getInstance(Context context){
-       if (instance == null){
-            instance = Room.databaseBuilder(context.getApplicationContext(),
-           VaccineVaccinedDatabase.class, "PostoX").build();
-       }
-       return instance;
-   }
 }
